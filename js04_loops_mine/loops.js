33ec210c3d0e995aa1922df6dc8933f5e1619b29
Ella <ekosardata@gmail.com>
2022-09-27 15:56:12 +0200

    updated

--- js04_loops_mine/loops.js
+++ js04_loops_mine/loops.js
@@ -108,3 +108,12 @@ console.log('****** LOOPS *******');
   //     }
   //   }
   // } while(hak > 0)
+
+
+
+    //  console.log(isNaN("Hello")) 
+    //  console.log(isNaN(345)  ) 
+    //  console.log( isNaN('1')  )
+    //  console.log( isNaN(true) )
+    //  console.log( isNaN(false))
+    //   console.log(  isNaN(undefined) )
